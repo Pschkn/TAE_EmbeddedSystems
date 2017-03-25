4042626445659465df7e293c6b3ff98f98ce6014
si5sc9 <simon.schneider@stz-softwaretechnik.de>
2017-03-25 09:57:08 +0100

    added classes for json parser

---
+++ prototypes/src/Items.java
@@ -0,0 +1,29 @@
+public class Items {
+
+	private Double[] position;
+	private String distance;
+	private String title;
+	
+	
+	public Double[] getPosition() {
+		return position;
+	}
+	public void setPosition(Double[] position) {
+		this.position = position;
+	}
+	public String getDistance() {
+		return distance;
+	}
+	public void setDistance(String distance) {
+		this.distance = distance;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	
+	
+}
---
+++ prototypes/src/Results.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Results {
+
+	private List<Items> items = new ArrayList<Items>();
+
+	public List<Items> getItems() {
+		return items;
+	}
+
+	public void setItems(List<Items> items) {
+		this.items = items;
+	}
+	
+}
